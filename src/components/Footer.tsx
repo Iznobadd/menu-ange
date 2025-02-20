f3fd445c7c60c883744a71723e129eaa93aaa502
Iznobad <brandon.cebi.pro@gmail.com>
2025-02-20 23:27:03 +0100

    change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 const Footer = () => {
   return (
     <div className="fixed bottom-0 w-full bg-black/60 text-white text-[9px] text-center py-1 font-light tracking-wider">
-      © {new Date().getFullYear()} Copyright | VB Digital all rights reserved
+      © {new Date().getFullYear()} Copyright | VB Digital Nomad all rights
+      reserved
     </div>
   );
 };
