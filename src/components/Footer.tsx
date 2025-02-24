848af20f48baa4630c28a9e5aa18ab1b9db25490
Brandon CEBI <brandon.cebi.pro@gmail.com>
2025-02-24 13:29:21 +0100

    add O footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="fixed bottom-0 w-full bg-black/60 text-white text-[9px] text-center py-1 font-light tracking-wider">
       © {new Date().getFullYear()} Copyright |
       <a href="mailto:contact@nomaado.com" className="px-2">
-        NOMAADO
+        NOMAADŌ
       </a>
       All Rights Reserved
     </div>
